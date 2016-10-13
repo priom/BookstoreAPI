40c106144fc11fd9a4737cb16c938a535c33862a
priom <priom.galaxy@gmail.com>
2016-10-13 17:08:30 -0300

    sample bookstore nodejs rest api

---
+++ app.js
@@ -0,0 +1,114 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+app.use(bodyParser.json());
+
+Genre = require('./models/genre');
+Book = require('./models/book');
+
+// connect mongoose
+mongoose.connect('mongodb://localhost/bookstore');
+var db = mongoose.connection;
+
+//home page
+app.get('/', function (req, res) {
+    res.send('Please use /api/book or /api/genres.');
+});
+
+//genres api
+app.get('/api/genres', function (req, res) {
+    Genre.getGenres(function (err, genres) {
+       if (err) {
+           throw err;
+       }
+       res.json(genres);
+    });
+});
+
+//add genre via api
+app.post('/api/genres', function (req, res) {
+    var genre = req.body;
+    Genre.addGenre(genre, function (err, genre) {
+        if (err) { throw err; }
+
+        res.json(genre);
+    });
+});
+
+//update genre via api
+app.put('/api/genres/:_id', function (req, res) {
+    var id = req.params._id;
+    var genre = req.body;
+    Genre.updateGenre(id, genre, {}, function (err, genre) {
+        if (err) { throw err; }
+
+        res.json(genre);
+    });
+});
+
+//delete genre via api
+app.delete('/api/genres/:_id', function (req, res) {
+    var id = req.params._id;
+    Genre.deleteGenre(id, function (err, genre) {
+        if (err) { throw err; }
+
+        res.json(genre);
+    });
+});
+
+
+//books api
+app.get('/api/books', function (req, res) {
+    Book.getBooks(function (err, books) {
+        if (err) { throw err; }
+
+        res.json(books);
+    });
+});
+
+//books api by ID
+app.get('/api/books/:_id', function (req, res) {
+    Book.getBookById(req.params._id, function (err, book) {
+        if (err) { throw err; }
+        res.json(book);
+    });
+});
+
+//add book via api
+app.post('/api/books', function (req, res) {
+    var book = req.body;
+    Book.addBook(book, function (err, book) {
+        if (err) { throw err; }
+
+        res.json(book);
+    });
+});
+
+//update book via api
+app.put('/api/books/:_id', function (req, res) {
+    var id = req.params._id;
+    var book = req.body;
+    Book.updateBook(id, book, {}, function (err, book) {
+        if (err) { throw err; }
+
+        res.json(book);
+    });
+});
+
+
+//delete genre via api
+app.delete('/api/books/:_id', function (req, res) {
+    var id = req.params._id;
+    Book.deleteBook(id, function (err, book) {
+        if (err) { throw err; }
+
+        res.json(book);
+    });
+});
+
+
+//
+app.listen(4000);
+console.log('running on port 4000');
